028839a3483eeb0a33d25132fc015a96827fb290
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-07 20:24:08 +0530

    adding logic to close display

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import time
 import random
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")
+
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
 GREEN_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_green_small.png'))
 BLUE_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_blue_small.png'))
@@ -18,3 +22,16 @@ YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'
 
 # Background
 BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                run = False
+
+main()
