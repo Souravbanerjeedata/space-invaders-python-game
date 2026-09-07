3960ae148ac4c070574d4440acbf1836aa435823
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-07 19:49:55 +0530

    importing modules and creating all contant variables

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import os
+import time
+import random
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
+GREEN_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_green_small.png'))
+BLUE_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_blue_small.png'))
+
+# PLAYER SHIP
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
+GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
+BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
+YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
+
+# Background
+BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
