1161f968f5abe22f572c995519c16b1f9c3b613f
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-08 12:48:44 +0530

    replacing player ship rectangle with a ship image and adjusting it's size and making sure ship does not go off the screen

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ GREEN_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_gre
 BLUE_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_blue_small.png'))
 
 # PLAYER SHIP
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png')), (60, 50))
 
 # Lasers
 RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
@@ -35,8 +35,20 @@ class Ship:
         self.cool_down_counter = 0
 
     def draw(self, window):
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 40, 40))
+        window.blit(self.ship_img, (self.x, self.y))
 
+    def get_width(self):
+        return self.ship_img.get_width()
+    def get_height(self):
+        return self.ship_img.get_height()
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACESHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
 
 def main():
     run = True
@@ -45,7 +57,7 @@ def main():
     lives = 5
     main_font = pygame.font.SysFont('comicsans', 20)
 
-    ship = Ship(235, 450)
+    player = Player(235, 450)
 
     player_vel = 5
 
@@ -54,10 +66,10 @@ def main():
         live_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
 
-        WIN.blit(live_label, (5, 5))
-        WIN.blit(level_label, (WIDTH - level_label.get_width() - 5, 5))
+        WIN.blit(live_label, (7, 7))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 7, 7))
 
-        ship.draw(WIN)
+        player.draw(WIN)
 
         pygame.display.update() 
 
@@ -70,13 +82,13 @@ def main():
                 run = False
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and ship.x - player_vel > 0:
-            ship.x -= player_vel
-        if keys[pygame.K_RIGHT] and ship.x + player_vel + 40 < WIDTH:
-            ship.x += player_vel
-        if keys[pygame.K_UP] and ship.y - player_vel > 0:
-            ship.y -= player_vel
-        if keys[pygame.K_DOWN] and ship.y + player_vel + 40 < HEIGHT:
-            ship.y += player_vel
+        if keys[pygame.K_LEFT] and player.x - player_vel > 0:
+            player.x -= player_vel
+        if keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < WIDTH:
+            player.x += player_vel
+        if keys[pygame.K_UP] and player.y - player_vel > 0:
+            player.y -= player_vel
+        if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
+            player.y += player_vel
 
 main()
